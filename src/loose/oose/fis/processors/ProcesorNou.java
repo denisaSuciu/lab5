9182c1bb04fce20511bbc88f55a0dbf5e29b5eca
denisaSuciu <deniisaoanaa@gmail.com>
2020-03-20 14:19:14 +0200

    Updated Branch1

---
+++ src/loose/oose/fis/processors/ProcesorNou.java
@@ -0,0 +1,9 @@
+package loose.oose.fis.processors;
+
+import loose.oose.fis.documents.Document;
+
+import java.util.ArrayList;
+
+public interface ProcesorNou {
+    int proceseaza(ArrayList<Document> documente);
+}
